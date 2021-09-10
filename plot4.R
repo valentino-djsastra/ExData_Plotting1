637ee42f9bb9f4c22ed3a08fddbcc7bdb94e3037
valentino-djsastra <78872249+valentino-djsastra@users.noreply.github.com>
2021-09-10 14:07:44 +0700

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,27 @@
+# Download File 
+tem <- tempfile() 
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",tem)
+data <- read.table(unz(tem,"household_power_consumption.txt"), sep=";", na="?", header = T, colClasses = 
+                         c("character", 'character', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric'))
+unlink(tem)
+
+#Filter Dates and convert to POSIX
+data <- data[which(data$Date == '2/2/2007' | data$Date=='1/2/2007'), ]
+data$POSIX <-as.POSIXlt.character(paste(data$Date,data$Time),format = "%d/%m/%Y %H:%M:%S")
+
+#Generate Plot 4
+png(filename="plot4.png",width = 640, height = 640)
+par(mfrow = c(2, 2))
+
+with(data, plot(POSIX, Global_active_power, type = "n", xlab = "", ylab = "Global Active Power (kilowatts)"))
+with(data, lines(POSIX, Global_active_power))
+
+with(data, plot(POSIX, Voltage, type = "l", xlab = "datetime", ylab = "Voltage"))
+
+with(data, plot(POSIX, Sub_metering_1, type = "l", col = "black", xlab = "", ylab = "Energy sub metering"))
+with(data, lines(POSIX, Sub_metering_2, col = 'red'))
+with(data, lines(POSIX, Sub_metering_3, col = 'blue'))
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c('black', 'red', 'blue'), lty = 1, bty = "n")
+
+with(data, plot(POSIX, Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power"))
+dev.off()
